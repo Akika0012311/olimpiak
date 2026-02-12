1961baf44c9100b8f3b253b92eb52e3783acdd60
Akika0012311 <komaromiakosbendeguz@turr.hu>
2026-02-12 12:55:14 +0100

    modifid

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const pMBnt = document.getElementById("push-me-btn");
+const sideMenuBtn = document.getElementById("menu-btn");
+const sideMenuBtn2 = document.getElementById("menu-btn2");
+const sideMenu = document.getElementById("side-menu-div");
 
 pMBnt.addEventListener("click", function() {
     PushMeBntOnClick();
@@ -12,4 +15,24 @@ function PushMeBntOnClick() {
 
 function PushMeBntSetBack() {
     pMBnt.style.border = "3px solid rgb(161, 161, 161)";
+}
+
+
+
+
+sideMenuBtn.addEventListener("click", function() {
+    OnClickSideMenuBtn();
+})
+
+function OnClickSideMenuBtn() {
+    sideMenu.style.transform = "translateX(-100%)";
+}
+
+
+sideMenuBtn2.addEventListener("click", function() {
+    OnClickSideMenuBtn2();
+})
+
+function OnClickSideMenuBtn2() {
+    sideMenu.style.transform = "translateX(0%)";
 }
